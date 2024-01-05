80c0ccb7300db9efe14d34f51110b727dacac68e
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-01-05 16:22:24 +0200

    update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,40 +1,74 @@
-import iziToast from 'izitoast';
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const section = document.querySelector('section');
 
+section.insertAdjacentHTML('afterend', `<form class="form">
+  <label class="delay-label">
+    Delay (ms)
+    <input type="number" name="delay" required />
+  </label>
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
+  <fieldset>
+    <legend>State</legend>
+    <label class="label-fulfilled">
+      <input type="radio" name="state" value="fulfilled" required />
+      Fulfilled
+    </label>
+    <label class="label-rejected">
+      <input type="radio" name="state" value="rejected" required />
+      Rejected
+    </label>
+  </fieldset>
 
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  <button type="submit">Create notification</button>
+</form>`);
 
-    const delayInput = form.querySelector('[name="delay"]');
-    const stateInput = form.querySelector('[name="state"]:checked');
+const form = document.querySelector('.form');
 
-    const delay = parseInt(delayInput.value, 10);
-    const state = stateInput.value;
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
-    try {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (state === 'fulfilled') {
-            resolve(`✅ Fulfilled promise in ${delay}ms`);
-          } else if (state === 'rejected') {
-            reject(`❌ Rejected promise in ${delay}ms`);
-          }
-        }, delay);
-      });
+  const delayInput = document.querySelector('[name="delay"]');
+  const stateInput = document.querySelector('[name="state"]:checked');
 
+  if (!delayInput || !stateInput) {
+    return;
+  }
+
+  const delay = parseInt(delayInput.value);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const state = stateInput.value;
+
+      if (state === 'rejected') {
+        reject('');
+      } else {
+        resolve('');
+      }
+    }, delay);
+  });
+
+  promise
+    .then(success => {
       iziToast.success({
-        title: 'Success',
-        message: `✅ Fulfilled promise in ${delay}ms`,
+        title: 'OK',
+        titleColor: '#FFF',
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#FFF',
+        position: 'topRight',
+        backgroundColor: '#59A10D'
       });
-    } catch (error) {
+    })
+    .catch(error => {
       iziToast.error({
         title: 'Error',
-        message: `❌ Rejected promise in ${delay}ms`,
+        titleColor: '#FFF',
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: '#FFF',
+        position: 'topRight',
+        backgroundColor: '#EF4040'
       });
-    }
-  });
+    });
 });
