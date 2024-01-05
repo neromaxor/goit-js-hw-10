94decc18632f8af1117761bd18b91524a23279a8
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-01-05 14:22:43 +0000

    Deploying to gh-pages from @ neromaxor/goit-js-hw-10@80c0ccb7300db9efe14d34f51110b727dacac68e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,21 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{i as o}from"./assets/vendor-651d7991.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".form");t.addEventListener("submit",async i=>{i.preventDefault();const n=t.querySelector('[name="delay"]'),a=t.querySelector('[name="state"]:checked'),e=parseInt(n.value,10),s=a.value;try{await new Promise((r,c)=>{setTimeout(()=>{s==="fulfilled"?r(`✅ Fulfilled promise in ${e}ms`):s==="rejected"&&c(`❌ Rejected promise in ${e}ms`)},e)}),o.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}catch{o.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})}})});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{i as r}from"./assets/vendor-651d7991.js";const i=document.querySelector("section");i.insertAdjacentHTML("afterend",`<form class="form">
+  <label class="delay-label">
+    Delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+
+  <fieldset>
+    <legend>State</legend>
+    <label class="label-fulfilled">
+      <input type="radio" name="state" value="fulfilled" required />
+      Fulfilled
+    </label>
+    <label class="label-rejected">
+      <input type="radio" name="state" value="rejected" required />
+      Rejected
+    </label>
+  </fieldset>
+
+  <button type="submit">Create notification</button>
+</form>`);const n=document.querySelector(".form");n.addEventListener("submit",s=>{s.preventDefault();const l=document.querySelector('[name="delay"]'),o=document.querySelector('[name="state"]:checked');if(!l||!o)return;const e=parseInt(l.value);new Promise((t,a)=>{setTimeout(()=>{o.value==="rejected"?a(""):t("")},e)}).then(t=>{r.success({title:"OK",titleColor:"#FFF",message:`Fulfilled promise in ${e}ms`,messageColor:"#FFF",position:"topRight",backgroundColor:"#59A10D"})}).catch(t=>{r.error({title:"Error",titleColor:"#FFF",message:`Rejected promise in ${e}ms`,messageColor:"#FFF",position:"topRight",backgroundColor:"#EF4040"})})});
 //# sourceMappingURL=commonHelpers2.js.map
